2a825231a81fa77da46c24047a1eebf840a330c2
Sabrina da Motta Café <sabis476@gmail.com>
2025-05-26 21:05:50 -0300

    🚀 Projeto inicial - Spring MVC com Thymeleaf

---
+++ mvc-admin/src/main/java/com/alerta_sp/mvc_admin/MvcAdminApplication.java
@@ -0,0 +1,13 @@
+package com.alerta_sp.mvc_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcAdminApplication.class, args);
+	}
+
+}
